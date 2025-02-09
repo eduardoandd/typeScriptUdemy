ee008cd12d17bd14bc40c0553c0021db5406871f
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-02-09 11:35:43 -0300

    Finalizando seção 2: conceitos básicos, inferência e annotations

---
+++ 1_fundamentos/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const teste = 'Teste'; // inferência
+const testeAno = 'Teste'; // annotation
+// Desafio
+let n1 = 1;
+let n1String = n1.toString();
+console.log(`Converção ${n1String}`);
---
+++ 1_fundamentos/index.ts
@@ -0,0 +1,13 @@
+const teste:string = 'Teste' // inferência
+const testeAno = 'Teste' // annotation
+
+
+// Desafio
+    let n1 = 1
+    let n1String=n1.toString()
+
+    console.log(`Converção ${n1String}`);
+    
+
+
+
