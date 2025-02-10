d042b448edcd86be5bef9f6219db9e2879ca6d69
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-02-10 11:06:18 -0300

    union types, type alias, interfaces, literal types, non-null

--- 3_Avançando_em_tipos/index.js
+++ 3_Avançando_em_tipos/index.js
@@ -44,3 +44,59 @@ function showNumbers(a, b) {
 }
 showNumbers(2, 4);
 showNumbers(1);
+// 39. Validação de parâmetros opcionais
+function advancedGreeting(firstName, lastName) {
+    console.log(firstName);
+    if (lastName !== undefined) {
+        console.log(lastName);
+    }
+}
+advancedGreeting('Eduardo', 'Andrade');
+advancedGreeting('Eduardo');
+// 40. Union types
+let union;
+union = 'tESTE';
+union = 2;
+// 41. avançando com Union types
+function unionsCondition(par) {
+    if (typeof (par) === 'string') {
+        console.log('string');
+    }
+    else {
+        console.log('number');
+    }
+}
+unionsCondition(union);
+function showId(id) {
+    console.log(id);
+}
+showId(1);
+showId('teste');
+function showCoords(obj) {
+    console.log(obj.x, obj.y, obj.z);
+}
+const coordObj = {
+    x: 0,
+    y: 0,
+    z: 0
+};
+showCoords(coordObj);
+const somePerson = {
+    name: 'Eduardo',
+    age: 0
+};
+// 45. Literal types
+function showDirection(direction) {
+    console.log(direction);
+}
+showDirection('Left');
+// 46. Non-null assertion Operator
+console.log();
+// 47. BigInt
+let n;
+n = 10000n;
+//48. Symbol
+let symbolA = Symbol('a');
+let symbolB = Symbol('a');
+console.log(symbolA == symbolB);
+console.log(symbolA === symbolB);
--- 3_Avançando_em_tipos/index.ts
+++ 3_Avançando_em_tipos/index.ts
@@ -35,7 +35,9 @@ setTimeout(function(){
 // 37. Tipos de objetos
 const objCoord = {
     x:394,
-    y:279.9
+    y:279.9,
+    z:'zero'
+
 }
 
 function passCords(coord: {x:number, y:number}){
@@ -55,6 +57,104 @@ function showNumbers(a: number, b?:number){
 }
 showNumbers(2,4)
 showNumbers(1)
+
+
+// 39. Validação de parâmetros opcionais
+function advancedGreeting(firstName: string, lastName?:string){
+    console.log(firstName);
+    if(lastName !== undefined){
+        console.log(lastName);
+    }
+}
+advancedGreeting('Eduardo','Andrade')
+advancedGreeting('Eduardo')
+
+// 40. Union types
+let union: string | number
+union = 'tESTE' 
+union = 2
+
+// 41. avançando com Union types
+function unionsCondition(par: string | number){
+    if(typeof(par) === 'string'){
+        console.log('string');
+    }
+    else{
+        console.log('number');
+        
+    }
+}
+unionsCondition(union)
+
+// 42. Type alias
+type ID = number | string
+
+function showId(id:ID){
+    console.log(id);
+}
+
+showId(1)
+showId('teste')
+
+
+// 43. Introdução as interfaces
+
+interface Point {
+    x:number
+    y:number
+    z:number
+}
+
+function showCoords(obj:Point){
+    console.log(obj.x,obj.y,obj.z); 
+}
+
+const coordObj: Point = {
+    x: 0,
+    y: 0,
+    z: 0
+}
+
+showCoords(coordObj)
+
+// 44. Type Alias x Interface
+    interface Person{
+        name:string
+    }
+    const somePerson:Person = {
+        name: 'Eduardo',
+        age: 0
+    }
+
+    interface Person {
+        age:number
+    }
+    
+// 45. Literal types
+function showDirection(direction: 'Left' | 'Right' | 'center'){
+    console.log(direction);    
+}
+
+showDirection('Left')
+
+// 46. Non-null assertion Operator
+console.log();
+
+// 47. BigInt
+let n:bigint
+n=10000n
+
+//48. Symbol
+let symbolA:symbol = Symbol('a')
+let symbolB:symbol = Symbol('a')
+
+console.log(symbolA == symbolB);
+console.log(symbolA === symbolB);
+
+
+
+
+
 
 
 
